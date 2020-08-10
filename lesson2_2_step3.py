1c0f771e3a6da81e76c1a508d6c101e198dd8f78
Anastasia999 <anastasiya.leonovich@mail.ru>
2020-08-10 22:01:33 +0700

    RRT

---
+++ lesson2_2_step3.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+try:
+    link = "http://suninjuly.github.io/selects1.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Ваш код, который заполняет обязательные поля
+    num1_element = browser.find_element_by_id("num1")
+    num1 = int(num1_element.text)
+    num2_element = browser.find_element_by_id("num2")
+    num2 = int(num2_element.text)
+    answer = str(num2+num1)
+
+    browser.find_element_by_tag_name("select").click()
+    browser.find_element_by_id("dropdown").send_keys(answer)
+
+    submit = browser.find_element_by_class_name("btn.btn-default")
+    submit.click()
+
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
